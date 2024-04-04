2dea0a5ebbcb703c7c5eca105a7ad1fed4a66380
scole02 <samcole107@gmail.com>
2024-04-03 23:29:04 -0700

    Completed Lab 6 head

--- catkin_ws/src/fetch-picker/applications/scripts/head_demo.py
+++ catkin_ws/src/fetch-picker/applications/scripts/head_demo.py
@@ -1,33 +1,33 @@
 #! /usr/bin/env python
 
 import rospy
-
+import robot_api
 
 def print_usage():
     # NOTE: We don't expect you to implement look_at for Kuri
     # But if you do, show us because that would be impressive ;)
     # `eyes`, naturally, is Kuri only.
-    print 'Usage:'
-    print '    rosrun applications head_demo.py look_at FRAME_ID X Y Z'
-    print '    rosrun applications head_demo.py pan_tilt PAN_ANG TILT_ANG'
-    print '    rosrun applications head_demo.py eyes ANG'
-    print 'Examples:'
-    print '    rosrun applications head_demo.py look_at base_link 1 0 0.3'
-    print '    rosrun applications head_demo.py pan_tilt 0 0.707'
-    print '    rosrun applications head_demo.py eyes .50'
-
+    print('Usage:')
+    print('    rosrun applications head_demo.py look_at FRAME_ID X Y Z')
+    print('    rosrun applications head_demo.py pan_tilt PAN_ANG TILT_ANG')
+    print('    rosrun applications head_demo.py eyes ANG')
+    print('Examples:')
+    print('    rosrun applications head_demo.py look_at base_link 1 0 0.3')
+    print('    rosrun applications head_demo.py pan_tilt 0 0.707')
+    print('    rosrun applications head_demo.py eyes .50')
 
 def wait_for_time():
-    """Wait for simulated time to begin.
-    """
+    """Wait for simulated time to begin."""
     while rospy.Time().now().to_sec() == 0:
         pass
-
 
 def main():
     rospy.init_node('head_demo')
     wait_for_time()
     argv = rospy.myargv()
+
+    head = robot_api.Head()
+
     if len(argv) < 2:
         print_usage()
         return
@@ -37,24 +37,25 @@ def main():
         if len(argv) < 6:
             print_usage()
             return
-        frame_id, x, y, z = argv[2], float(argv[3]), float(argv[4]), float(
-            argv[5])
-        rospy.logerr('Not implemented.')
+        frame_id, x, y, z = argv[2], float(argv[3]), float(argv[4]), float(argv[5])
+        head.look_at(frame_id, x, y, z)  # implemented
+
     elif command == 'pan_tilt':
         if len(argv) < 4:
             print_usage()
             return
         pan, tilt = float(argv[2]), float(argv[3])
-        rospy.logerr('Not implemented.')
+        head.pan_tilt(pan, tilt)  # implemented
+
     elif command == 'eyes':
         if len(argv) < 3:
             print_usage()
             return
         angle = float(argv[2])
-        rospy.logerr('Not implemented.')
+        rospy.logerr('Not implemented.')  # Fetch doesn't have eyes. 
+    
     else:
         print_usage()
-
 
 if __name__ == '__main__':
     main()
